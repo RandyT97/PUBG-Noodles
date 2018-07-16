850d1e788d394a1aee310e047639d2ac5a7b2c17
warren1215 <warrenoliver1215@gmail.com>
2018-07-15 22:17:51 -0400

    fixed django

--- djreact/urls.py
+++ djreact/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('api/', include('exapp.urls')),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
