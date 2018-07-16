bf70ad10d398dd6ebebbf55479b1eee3f410cd9e
warren1215 <warrenoliver1215@gmail.com>
2018-07-16 00:06:58 -0400

    its working

--- Django/noodle/static/index.js
+++ Django/noodle/static/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+const element = <Welcome name="world" />;
+ReactDOM.render(
+  element,
+  document.getElementById('react')
+);
